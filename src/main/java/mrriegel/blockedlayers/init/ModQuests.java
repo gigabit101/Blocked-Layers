b848a778ebcda0e5468e75353b7447eba0a20001
gigabit101 <thegigabit101@gmail.com>
2016-04-26 00:29:58 +0100

    removed junk

--- src/main/java/mrriegel/blockedlayers/init/ModQuests.java
+++ src/main/java/mrriegel/blockedlayers/init/ModQuests.java
@@ -8,9 +8,6 @@ public class ModQuests
 {
 	public static void init()
 	{
-//		BlockedLayersApi.questList.add(new Quest("apple", "eat", "apple", "minecraft", "This was added with the API", 64, 0, 6, 0));
 		BlockedLayersApi.addQuest("apple", "eat", "apple", "minecraft", "This was added with the API", 64, 0, 1, 0);
-
-//		BlockedLayers.instance.questList.add(new Quest("apple", "eat", "apple", "minecraft", "This was added with the API", 64, 0, 6, 0));
 	}
 }
